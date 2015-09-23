e066be79633d0975257b1586faabe1f6ad12828f
Ron Kurr <kurr@kurron.org>
2015-09-23 18:07:49 +0000

    Added JVM monitoring

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Use host networking to avoid having to map every hostname needed for the simulation
-docker run --detach=true --name=mongodb-service --net=host --publish=8100:8100 kurron/monitor-mongodb:latest
+docker run --detach=true --name=mongodb-service --net=host --publish=8100:8100 --env=NEW_RELIC_APP_NAME=mongodb-service --env=NEW_RELIC_HOST_DISPLAY_NAME=${HOSTNAME} kurron/monitor-mongodb:latest
 docker logs --follow=true --tail=25 mongodb-service
